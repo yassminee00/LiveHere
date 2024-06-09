37cc89abf2b1192eaa7f357f78952dce6f14cfed
yassminee <142147424+yassminee00@users.noreply.github.com>
2024-06-09 14:46:07 +0100

    Update Team.js

--- frontend/livehere/src/components/About/Team.js
+++ frontend/livehere/src/components/About/Team.js
@@ -1,6 +1,9 @@
 
 import React from "react";
 import { FaFacebook, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import Emma from "../../assets/Emma.jpeg";
+import Milly from "../../assets/Milly.jpeg";
+import Robert from "../../assets/Robert.jpeg";
 import yassmine from "../../assets/yassmine.jpg";
 
 const Team = (props) => {
@@ -15,10 +18,10 @@ const Team = (props) => {
       <div className="flex flex-col justify-center  px-10 md:px-16 xl:px-[180px] sm:grid sm:grid-cols-2  lg:grid-cols-4 gap-10">
         <div className="TeamImageBox text-center">
           <div className="bg-zinc-800 rounded-full rounded-br-none w-[220px] inline-block h-[220px] mr-2 overflow-hidden">
-            <img className="w-full object-cover" src={Zeliq} alt="Zeliq"></img>
+            <img className="w-full object-cover" src={yassmine} alt="yassmine"></img>
           </div>
           <div className="inline-block">
-            <h5 className="text-[24px] sm:text-[20px]">Zelipha U</h5>
+            <h5 className="text-[24px] sm:text-[20px]">yassmine U</h5>
             <div className="flex text-[30px]">
               <FaFacebook className="pr-2 hover:scale-105 cursor-pointer" />
               <FaTwitter className="pr-2 hover:scale-110 cursor-pointer" />
