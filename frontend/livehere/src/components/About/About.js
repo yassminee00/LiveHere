b15ac755fbc6d151d26680e971cb2ceb3e87922c
yassminee <142147424+yassminee00@users.noreply.github.com>
2024-06-10 06:55:13 +0100

    Update About.js

--- frontend/livehere/src/components/About/About.js
+++ frontend/livehere/src/components/About/About.js
@@ -13,7 +13,7 @@ function About() {
       <Service
         label="About Us"
         header="We Provide The Best Property For You"
-        text="With our vast search tool, you can find yourr dream home from any location in the world. All we do is help you find the place, meet the agents, and seal the deal. There's no extra commission for our service."
+        text="With our vast search tool, you can find your dream home from anywhere in the world. All we do is help you find the place, meet the agents, and seal the deal. There's no extra commission for our service."
       />
 
       <SearchBar header="Let's Find A Property That Meets Your Need" />
