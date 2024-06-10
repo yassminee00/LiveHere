677836912167e5ab39fa7cc328f289bc714b1e21
yassminee <142147424+yassminee00@users.noreply.github.com>
2024-06-10 07:02:42 +0100

    Update RegisterAgent.js

--- frontend/livehere/src/components/Templates/RegisterAgent.js
+++ frontend/livehere/src/components/Templates/RegisterAgent.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -11,7 +10,7 @@ function RegisterAgent() {
 
       <div className="w-full mx-auto p-10 text-center">
         <h2 className="pb-4">Do you have what it takes to consult for property management?</h2>
-        <h5>Register with your license and let's support you to build your career.</h5>
+        <h5>Register with your license and let's support you in building your career.</h5>
       </div>
 
       <Form />
